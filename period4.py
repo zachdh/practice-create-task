e81ace211e23d53808f752d26bb09a30ccebf00b
eth4npham Pham <ethanpham0918@gmail.com>
2023-09-21 18:18:39 +0000

    adventrue simulator

--- period4.py
+++ period4.py
@@ -1,10 +1,35 @@
-inventory = ("BOW", "SWORD", "WAND", "BOMB")
+inventory = ["BOW", "SWORD", "WAND", "BOMB"]
 
-input("WELCOME TO ADVENTURE SIMULATOR! WHAT IS YOUR NAME TRAVELER?")
+enemy_items = {
+    "Gibby the Goblin": "SWORD",
+    "Wanda the Witch": "WAND",
+    "Killor the Knight": "BOW",
+    "Gregory the Giant": "BOMB"
+}
 
-print("AS YOU ARE TRAVELING YOU COME ACORSS MERLIN THE GREAT, HE APPROACHES YOU WITH A QUEST TO DEFEAT THE FOUR GREAT CREATURES WHO HAVE BEEN TERRIFYING THE LANDS. WILL YOU BE THE FIRST TO COME BACK ALIVE? ")
+def adventurer_simulator():
+    Player_name = input("WELCOME TO ADVENTURE SIMULATOR! WHAT IS YOUR NAME TRAVELER? ")
+   
+    print("AS YOU ARE TRAVELING YOU COME  ACROSS MERLIN THE GREAT, HE APPROACHES YOU WITH A QUEST TO DEFEAT THE FOUR GREAT CREATURES WHO HAVE BEEN TERRIFYING THE LANDS. WILL YOU BE THE FIRST TO COME BACK ALIVE? ")
+   
+    print("Here is your inventory ", inventory)
+   
+    print("Gibby the Giant stands in your path!")
+   
+    correct_item = enemy_items["Gibby the Goblin"]
+   
+    player_choice= input("Choose a weapon from your inventory to defeat the enemy!")
 
-def inventory(WEAPON_CHOICE, inventory):
-    
-    for i in range(len(inventory)):
-        str = "PICK "
+if player_choice == correct_item:
+
+    print("Congratulations", Player_name, ", you have defeated Gibby the Goblin. Prepare for the next enemy.")
+
+else: 
+
+    print:("Your", player_choice, "was no match for Gibby the Goblin. Gibby the Goblin sat on you and you died a horrible death.")
+
+    break
+
+adventurer_simulator()
+
+
